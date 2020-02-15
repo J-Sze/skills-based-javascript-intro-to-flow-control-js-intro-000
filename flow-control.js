704ef6659c612e7b2c3ee1f4d2e90a2e5d4cdf12
Jackie Sze <jackiesze0@gmail.com>
2020-02-15 21:19:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,11 +5,12 @@ return "You are a teenager!";
 }
 
 function teenager(age) {
-if (age >= 13 && age <= 19) {
-  return 'You are a teenager!';
-} else {
-  return 'You are not a teenager';
-}
+  if (age <= 19 && age => 13){
+    console.log ("You are a teenager!");
+  }
+  else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
