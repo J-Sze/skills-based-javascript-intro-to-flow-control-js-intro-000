764c557699812aa8917bae1e31c95c2e055eee42
Jackie Sze <jackiesze0@gmail.com>
2020-02-15 21:17:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,7 @@ age => 13 && age <= 19 ? console.log("You are a teenager") : "You are not a teen
 function switchAge(age) {
   switch(age) {
     case age >= 13 && age <= 19:
-      console.log('You are a teenager');
+      return 'You are a teenager';
       break;
       default: 
       return "you have an age";
